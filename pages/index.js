8d625b674d8094e317a791e3b815bb6c71a0f88f
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-11-18 08:57:00 -0700

    Initial Edits

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>The all new BabylonJS-Viewer!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the future of interactive data!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Introducing the BabylonJS-Viewer
         </p>
       </main>
 
