ea1211cf3e426590cbae083015d1eeb438a9980f
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-11-23 13:49:07 -0700

    Necips SpinningBoxes

--- components/SpinningBox.js
+++ components/SpinningBox.js
@@ -1,8 +1,6 @@
 import React, { useRef, useState } from 'react'
-import { Engine, Scene, useBeforeRender, useClick, useHover } from 'react-babylonjs'
+import { useBeforeRender, useClick, useHover } from 'react-babylonjs'
 import { Vector3, Color3 } from '@babylonjs/core'
-import '@styles/globals.css'
-
 
 const DefaultScale = new Vector3(1, 1, 1);
 const BiggerScale = new Vector3(1.25, 1.25, 1.25);
@@ -39,23 +37,4 @@ const SpinningBox = (props) => {
     </box>);
 }
 
-export default function App() {
-    return (
-        <div>
-            <Engine antialias adaptToDeviceRatio canvasId='renderCanvas'>
-                <Scene>
-                    <arcRotateCamera name="camera1" target={Vector3.Zero()} alpha={Math.PI / 2} beta={Math.PI / 4} radius={8} />
-                    <hemisphericLight name='light1' intensity={0.7} direction={Vector3.Up()} />
-                    <SpinningBox name='left' position={new Vector3(-2, 0, 0)}
-                        color={Color3.FromHexString('#EEB5EB')} hoveredColor={Color3.FromHexString('#C26DBC')}
-                    />
-                    <SpinningBox name='right' position={new Vector3(2, 0, 0)}
-                        color={Color3.FromHexString('#C8F4F9')} hoveredColor={Color3.FromHexString('#3CACAE')}
-                    />
-                </Scene>
-            </Engine>
-        </div>
-    );
-}
-
-export { App }
+export { SpinningBox }
