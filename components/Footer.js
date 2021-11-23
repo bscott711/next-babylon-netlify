fc0859feace71a6e58eb17bf39c142a96c049a13
bscott711 <40175801+bscott711@users.noreply.github.com>
2021-11-23 12:16:12 -0700

    Dev linting requirements

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,18 @@
+import Image from 'next/image';
 import styles from '@styles/Footer.module.css'
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> by <b>&nbsp;MinesBioImaging</b>
+        Made with&nbsp;<Image
+          priority
+          src="/netliheart.svg"
+          alt={"Netlify Logo"}
+          className={styles.logo}
+          width={20}
+          height={20}
+        />&nbsp;by<b>&nbsp;MinesBioImaging</b>
       </footer>
     </>
   )
