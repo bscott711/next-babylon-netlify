ae99df7c5991f900096c0acd21f04cc6f23eb53c
bscott711 <40175801+bscott711@users.noreply.github.com>
2022-03-17 21:52:45 -0600

    Updated dHL6_d6

--- pages/dhl60_day6.js
+++ pages/dhl60_day6.js
@@ -5,7 +5,7 @@ export default function dHL60_day6() {
     return (
         <Layout title={'Babylon-Viewer'}>
             <strong>Day6 dHL60 motion</strong>
-            <Viewer canvasId={'renderCanvas'} root={'assets/dHL60_day6/'} prefix={'surface_'} startnum={1} numfiles={100} />
+            <Viewer canvasId={'renderCanvas'} root={'assets/dHL60_day6/'} prefix={'Surfaces_'} startnum={1} numfiles={100} />
         </Layout>
     )
 }
